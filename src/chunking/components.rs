b08b44cf8e6d9e46252d8c218a9fbc2ddc2a56fc
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2025-07-26 14:56:41 +0200

    Update components.rs

--- src/chunking/components.rs
+++ src/chunking/components.rs
@@ -4,18 +4,16 @@ use bevy::prelude::*;
 #[reflect(Component)]
 pub struct ChunkCenter;
 
-#[derive(Component, Reflect)]
-#[reflect(Component)]
+#[derive(Component)]
 pub struct CanSplit;
 
-#[derive(Component, Reflect)]
-#[reflect(Component)]
+#[derive(Component)]
 pub struct CanMerge;
 
 #[derive(Component, Debug, Clone, Copy, Reflect)]
 #[reflect(Component)]
 pub struct Chunk {
-    /// The level of detail, i.e. (0=Low, 1=Medium, 2=High).
+   /// The level of detail, e.g. (0=Low, 1=Medium, 2=High).
     pub level: u32,
     pub size: u32,
 }
