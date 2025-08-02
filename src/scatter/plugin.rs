5aeb8699ca6226cb66a2c5b08ed99312aaf766b3
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2025-08-03 00:49:59 +0200

    Update plugin.rs

--- src/scatter/plugin.rs
+++ src/scatter/plugin.rs
@@ -1,8 +1,8 @@
 use crate::chunking::systems::prelude::split;
 use crate::scatter::observers::*;
-use crate::scatter::prelude::*;
 use crate::scatter::systems::prelude::*;
 use bevy::prelude::*;
+use crate::prelude::*;
 
 pub struct ScatterPlugin;
 
@@ -12,6 +12,7 @@ impl Plugin for ScatterPlugin {
             .add_event::<Scatter<ScatterLayer>>()
             .add_event::<ScatterChunk>()
             .add_event::<ScatterResults>()
+            .add_event::<SplitChunk>()
             .add_observer(on_add_chunk)
             .add_observer(on_add_scatter_root)
             .add_observer(on_add_scatter_layer)
