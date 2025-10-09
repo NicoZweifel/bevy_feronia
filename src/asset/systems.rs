1f34135d3d1b524ec0f350fbdd011a6fb055407a
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2025-10-09 20:47:41 +0200

    chore: format

--- src/asset/systems.rs
+++ src/asset/systems.rs
@@ -53,19 +53,33 @@ impl Wind {
         ): WindData,
     ) -> Self {
         Wind {
-            strength: strength.map(|x| **x * self.strength).unwrap_or(self.strength),
-            micro_strength: micro_strength.map(|x| **x * self.micro_strength).unwrap_or(self.micro_strength),
+            strength: strength
+                .map(|x| **x * self.strength)
+                .unwrap_or(self.strength),
+            micro_strength: micro_strength
+                .map(|x| **x * self.micro_strength)
+                .unwrap_or(self.micro_strength),
             s_curve_strength: s_curve_strength
                 .map(|x| **x * self.s_curve_strength)
                 .unwrap_or(self.s_curve_strength),
-            s_curve_speed: s_curve_speed.map(|x| **x * self.s_curve_speed).unwrap_or(self.s_curve_speed),
+            s_curve_speed: s_curve_speed
+                .map(|x| **x * self.s_curve_speed)
+                .unwrap_or(self.s_curve_speed),
             s_curve_frequency: s_curve_frequency
                 .map(|x| **x)
                 .unwrap_or(self.s_curve_frequency),
-            bop_strength: bop_strength.map(|x| **x * self.bop_strength).unwrap_or(self.bop_strength),
-            bop_speed: bop_speed.map(|x| **x * self.bop_speed).unwrap_or(self.bop_speed),
-            twist_strength: twist_strength.map(|x| **x * self.twist_strength).unwrap_or(self.twist_strength),
-            bend_exponent: bend_exponent.map(|x| **x * self.bend_exponent).unwrap_or(self.bend_exponent),
+            bop_strength: bop_strength
+                .map(|x| **x * self.bop_strength)
+                .unwrap_or(self.bop_strength),
+            bop_speed: bop_speed
+                .map(|x| **x * self.bop_speed)
+                .unwrap_or(self.bop_speed),
+            twist_strength: twist_strength
+                .map(|x| **x * self.twist_strength)
+                .unwrap_or(self.twist_strength),
+            bend_exponent: bend_exponent
+                .map(|x| **x * self.bend_exponent)
+                .unwrap_or(self.bend_exponent),
             low_quality: low_quality.map(|_| true).unwrap_or(self.low_quality),
             ..*self
         }
