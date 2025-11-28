b2d4a22d9155b4226bb2623cea300860f8f11e4b
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2025-11-28 10:12:31 +0100

    chore: clarify comment

--- src/instancing/resources.rs
+++ src/instancing/resources.rs
@@ -22,7 +22,8 @@ pub(super) struct GlobalCullBuffer {
     pub buffer: Buffer,
 }
 
-// TODO: this leaks memory, but it'll take forever. Gotta implement a queue or sth like that.
+// TODO: this leaks memory, but it'll take forever.
+// Gotta implement a queue or sth like that to manage buffers which might be a good timne to fix this.
 #[derive(Resource, Default)]
 pub(super) struct GrassBufferCache {
     pub buffers: HashMap<Entity, Buffer>,
