1e469865e389a15f21805050c19f8b40215eeee0
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2026-02-14 21:15:08 +0100

    Update scatter_observer.rs

--- src/scatter/observers/scatter_observer.rs
+++ src/scatter/observers/scatter_observer.rs
@@ -17,7 +17,7 @@ pub fn scatter_observer<T>(
     let layer = trigger.layer;
     let Ok(scatter_items) = q_layer.get(layer) else {
         #[cfg(feature = "trace")]
-        warn!("ScatterLayer {layer} not found!");
+        debug!("ScatterLayer {layer} not found!");
         return;
     };
 
