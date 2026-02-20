3b447bf5f0b8f7b7aa6a373610c40cac2d736733
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2026-02-20 15:04:15 +0100

    Update example.rs

--- examples/utils/example.rs
+++ examples/utils/example.rs
@@ -263,7 +263,7 @@ pub fn setup_camera(mut cmd: Commands, asset_server: Res<AssetServer>, q_camera:
 
     cmd.spawn((
         (Camera::default(), Camera3d::default()),
-        (Center,CullComputeCamera),
+        (Center, CullComputeCamera),
         Hdr,
         Controller::default(),
         ColorGrading::default(),
